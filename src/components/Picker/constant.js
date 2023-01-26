6d2fad54d5c8295a0487170ad36b8455dedc684e
Evgeniya Zaikina <evgeniya.zaikina@progforce.com>
2023-01-26 10:50:22 +0300

    feat: dropdown button with select value

---
+++ src/components/Picker/constant.js
@@ -0,0 +1,24 @@
+export const DEFAULT_OPTIONS = [
+  {
+    key: '0',
+    value: '1',
+    label: 'Last 1 hr',
+  },
+  {
+    key: '1',
+    value: '24',
+    label: 'Last 24 hrs',
+  },
+  {
+    key: '3',
+    value: '72',
+    label: 'Last 3 days',
+  },
+  {
+    key: '7',
+    value: '168',
+    label: 'Last 7 days',
+  },
+];
+
+export const PLACEHOLDER = 'None';
--- src/components/Picker/index.js
+++ src/components/Picker/index.js
@@ -1,5 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { PropTypes } from 'prop-types';
+import styled from 'styled-components';
+import { DownOutlined } from '@ant-design/icons';
+import { Dropdown, Typography } from 'antd';
+import { DEFAULT_OPTIONS, PLACEHOLDER } from './constant';
 
-export const Picker = () => {
-  return <>Picker</>;
+const { Text } = Typography;
+
+export const Picker = ({
+  options = DEFAULT_OPTIONS,
+  placeholder = PLACEHOLDER,
+  onChange = undefined,
+}) => {
+  const [selectedOption, setSelectedOption] = useState();
+
+  const onClick = (e) => {
+    const newSelectedOption = options.find((option) => option.key === e.key);
+    setSelectedOption(newSelectedOption);
+    onChange && onChange(newSelectedOption);
+  };
+
+  return (
+    <Dropdown
+      menu={{
+        items: options,
+        onClick,
+      }}
+      trigger={['click']}
+    >
+      <DropdownButton>
+        <Text>{selectedOption?.label || placeholder}</Text>
+        <DownOutlined />
+      </DropdownButton>
+    </Dropdown>
+  );
 };
+
+Picker.propTypes = {
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      key: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      value: PropTypes.number.isRequired,
+    })
+  ),
+  placeholder: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+const DropdownButton = styled.button`
+  background-color: #ffffff;
+  display: inline-flex;
+  padding: 0 12px;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid rgba(2, 9, 28, 0.28);
+  border-radius: 4px;
+  min-width: 150px;
+  min-height: 40px;
+
+  &:focus {
+    cursor: pointer;
+  }
+`;
