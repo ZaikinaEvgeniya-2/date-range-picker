5bbf112bf4e03db3090d444e5ff91de5505d92cb
Evgeniya Zaikina <evgeniya.zaikina@progforce.com>
2023-01-27 16:11:13 +0300

    refactor: move file

--- src/Picker/index.js
+++ src/Picker/index.js
@@ -1,1 +1,82 @@
-export { Picker } from './Picker';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { PropTypes } from 'prop-types';
+import { DEFAULT_OPTIONS, EMPTY_OPTION, CUSTOM_OPTION } from './constant';
+import { OptionType } from './types';
+import { DropdownPicker } from './DropdownPicker';
+import { DateRangePicker } from './DateRangePicker';
+
+export const Picker = ({
+  options = DEFAULT_OPTIONS,
+  onChange,
+  value,
+  enableEmptySelection = true,
+  enableCustomDate = true,
+  ...props
+}) => {
+  const [selectedOption, setSelectedOption] = useState(value);
+
+  useEffect(() => {
+    setSelectedOption(value);
+  }, [value?.value]);
+
+  const onSelect = useCallback(
+    (newSelectedOption) => {
+      setSelectedOption(newSelectedOption);
+      onChange && onChange(newSelectedOption?.value, newSelectedOption);
+    },
+    [onChange]
+  );
+
+  const onCustomDateSelect = useCallback(
+    (dateRange) => {
+      onSelect({ ...CUSTOM_OPTION, value: dateRange });
+    },
+    [onSelect]
+  );
+
+  const onClear = useCallback(() => {
+    onSelect();
+  }, [onSelect]);
+
+  const items = useMemo(() => {
+    let res = options;
+
+    if (enableEmptySelection) {
+      res = [EMPTY_OPTION, ...res];
+    }
+
+    if (enableCustomDate) {
+      res = [...res, CUSTOM_OPTION];
+    }
+
+    return res;
+  }, [enableCustomDate, enableEmptySelection, options]);
+
+  if (selectedOption?.key === CUSTOM_OPTION.key) {
+    return (
+      <DateRangePicker
+        value={selectedOption?.value}
+        onChange={onCustomDateSelect}
+        onClose={onClear}
+        {...props}
+      />
+    );
+  }
+
+  return (
+    <DropdownPicker
+      options={items}
+      selectedOption={selectedOption}
+      placeholder={EMPTY_OPTION.label}
+      onSelect={onSelect}
+    />
+  );
+};
+
+Picker.propTypes = {
+  options: PropTypes.arrayOf(OptionType),
+  value: OptionType,
+  onChange: PropTypes.func,
+  enableEmptySelection: PropTypes.bool,
+  enableCustomDate: PropTypes.bool,
+};
