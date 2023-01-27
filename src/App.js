6b9ad44acd5c97dbd8bb044a2adbe3171993dd1e
Evgeniya Zaikina <evgeniya.zaikina@progforce.com>
2023-01-27 13:06:01 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Picker } from './Picker';
 import { CustomDaysDropdown } from './old';
 import './App.css';
 
 export const App = () => {
+  const [value, setValue] = useState();
+
+  console.log('CURRENT VALUE', value);
+
   return (
-    <div style={{ display: 'flex', width: '400px' }}>
-      <Picker />
+    <div style={{ width: '400px' }}>
+      <Picker value={value} onChange={setValue} />
       <CustomDaysDropdown />
     </div>
   );
